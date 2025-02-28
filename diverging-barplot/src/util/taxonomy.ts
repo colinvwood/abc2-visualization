b6359d628a748ddc7c463e9e8a8941d32da09167
Colin Wood <woodcolin38@gmail.com>
2025-02-28 15:52:51 -0700

    TaxonomyTree first implementation

--- diverging-barplot/src/util/taxonomy.ts
+++ diverging-barplot/src/util/taxonomy.ts
@@ -1,137 +1,256 @@
 import { tsv } from "d3-fetch";
+import * as d3 from "d3";
+
+type PlotDimensions = {
+    width: number;
+    margin: number;
+};
+export class TaxonomyPlot {
+    dimensions: PlotDimensions = {
+        width: 1700,
+        margin: 50,
+    };
+
+    root: d3.HierarchyNode<TaxonomyNode>;
+    treeLayout: d3.TreeLayout<any>;
+    link: d3.Link<any, any, any>;
+
+    constructor(root: d3.HierarchyNode<TaxonomyNode>) {
+        this.hideAllChildren(root);
+        this.root = root;
+        this.treeLayout = this.createTreeLayout();
+        this.link = this.createLinkGenerator();
+        this.createSvgGroups();
+    }
+
+    hideAllChildren(root: d3.HierarchyNode<TaxonomyNode>) {
+        root.descendants().forEach((node, i) => {
+            node._children = node.children;
+            node.id = i;
+            node.children = null;
+        });
+
+        root.links().forEach((node, i) => {});
+    }
+
+    createTreeLayout(): d3.TreeLayout<any> {
+        const taxonomyDepth = this.root.height;
+        const nodeWidth = this.dimensions.width / (taxonomyDepth + 1);
+        const nodeHeight = 20;
+        const treeLayout = d3.tree().nodeSize([nodeHeight, nodeWidth]);
+
+        return treeLayout;
+    }
+
+    createLinkGenerator(): d3.Link<any, any, any> {
+        return d3
+            .linkHorizontal()
+            .x((d) => d.y)
+            .y((d) => d.x);
+    }
+
+    createSvgGroups() {
+        const svg = d3
+            .select(".taxonomy svg")
+            .append("g")
+            .attr("class", "node-group");
+
+        svg.append("g").attr("class", "link-group");
+    }
+
+    addClickHandlers(selection: d3.Selection<any, any, any, any>) {
+        selection.on("click", (event, d) => {
+            d.children = d.children ? null : d._children;
+            this.render(event, d);
+        });
+    }
+
+    resizeSvg(): d3.Transition<any, any, any, any> {
+        // find lowest and highest node coordinates (along svg y axis)
+        let upperLimit = 0;
+        let lowerLimit = 0;
+        this.root.eachBefore((node) => {
+            if (node.x! < lowerLimit) lowerLimit = node.x!;
+            if (node.x! > upperLimit) upperLimit = node.x!;
+        });
+
+        const height = upperLimit - lowerLimit + 2 * this.dimensions.margin;
+
+        // resize the svg and readjust its viewbox to center the tree
+        const transition = d3
+            .select(".taxonomy svg")
+            .transition()
+            .duration(200)
+            .attr("width", this.dimensions.width)
+            .attr("height", height)
+            .attr("viewBox", [
+                -this.dimensions.margin,
+                lowerLimit - this.dimensions.margin,
+                this.dimensions.width,
+                height,
+            ]);
+
+        return transition;
+    }
+
+    drawNodes(
+        nodes: d3.HierarchyNode<TaxonomyNode>[],
+        source: d3.HierarchyNode<TaxonomyNode>,
+        transition: d3.Transition<any, any, any, any>,
+    ) {
+        const updateSelection = d3
+            .select(".node-group")
+            .selectAll(".taxonomy-node")
+            .data(nodes, (d) => d.id);
+
+        // create entering node groups
+        const enterSelection = updateSelection
+            .enter()
+            .append("g")
+            .attr("class", "taxonomy-node")
+            .attr("cursor", "pointer")
+            .attr("transform", (d) => `translate(${source.y0}, ${source.x0})`)
+            .attr("stroke-opacity", 1)
+            .attr("fill-opacity", 1);
+
+        enterSelection
+            .append("circle")
+            .attr("r", 3)
+            .attr("fill", "#555")
+            .attr("stroke-width", 10);
+
+        enterSelection
+            .append("text")
+            .text((d) => d.data.name)
+            .attr("dy", 4)
+            .attr("x", (d) => (d._children ? -5 : 5))
+            .attr("text-anchor", (d) => (d._children ? "end" : "start"))
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-width", 5)
+            .attr("stroke", "white")
+            .attr("paint-order", "stroke")
+            .attr("font-weight", "bold")
+            .attr("font-size", 14);
+
+        this.addClickHandlers(enterSelection);
+
+        // transition all nodes to their new positions
+        updateSelection
+            .merge(enterSelection)
+            .transition(transition)
+            .attr("transform", (d) => `translate(${d.y}, ${d.x})`)
+            .attr("fill-opacity", 1)
+            .attr("stroke-opacity", 1);
+
+        // transition exiting nodes to parents' new positions
+        updateSelection
+            .exit()
+            .transition(transition)
+            .remove()
+            .attr("transform", (d) => `translate(${source.y}, ${source.x})`)
+            .attr("fill-opacity", 0)
+            .attr("stroke-opacity", 0);
+    }
+
+    drawLinks(
+        links: d3.HierarchyLink<TaxonomyNode>[],
+        source: d3.HierarchyNode<TaxonomyNode>,
+        transition: d3.Transition<any, any, any, any>,
+    ) {
+        const updateSelection = d3
+            .select(".link-group")
+            .selectAll(".taxonomy-link")
+            .data(links, (d) => d.target.id);
+
+        // create entering link paths
+        const enterSelection = updateSelection
+            .enter()
+            .append("path")
+            .attr("class", "taxonomy-link")
+            .attr("fill", "none")
+            .attr("stroke", "#555")
+            .attr("stroke-opacity", 0.5)
+            .attr("stroke-width", 1.5)
+            .attr("d", (d) => {
+                const parentOld = { x: source.x0, y: source.y0 };
+                return this.link({ source: parentOld, target: parentOld });
+            });
+
+        // transition all links to their new shapes
+        updateSelection
+            .merge(enterSelection)
+            .transition(transition)
+            .attr("d", this.link);
+
+        // transition exiting links to parents' new
+        updateSelection
+            .exit()
+            .transition(transition)
+            .remove()
+            .attr("d", (d) => {
+                const parentNew = { x: source.x, y: source.y };
+                return this.link({ source: parentNew, target: parentNew });
+            });
+    }
+
+    render(event: MouseEvent | null, source: d3.HierarchyNode<TaxonomyNode>) {
+        // gather all expanded nodes and links
+        const nodes = this.root.descendants().reverse();
+        const links = this.root.links();
+
+        // assign coordinates to nodes
+        this.treeLayout(this.root);
+
+        const transition = this.resizeSvg();
+        this.drawLinks(links, source, transition);
+        this.drawNodes(nodes, source, transition);
+
+        // store the current positions for the next render
+        this.root.eachBefore((node) => {
+            node.x0 = node.x;
+            node.y0 = node.y;
+        });
+    }
+}
 
 export class TaxonomyNode {
     name: string;
-    featureIds: string[];
-    parent: TaxonomyNode | null;
+    featureIDs: string[];
     children: TaxonomyNode[];
-    data: object;
+    hierarchyNode: d3.HierarchyNode<TaxonomyNode> | null;
 
-    constructor(
-        name: string,
-        parent?: TaxonomyNode,
-        id?: string,
-        children?: TaxonomyNode[],
-        data?: object,
-    ) {
+    constructor(name: string) {
         this.name = name;
-        this.parent = parent ? parent : null;
-        this.featureIds = id ? [id] : [];
-        this.children = children ? children : [];
-        this.data = data ? data : {};
+        this.featureIDs = [];
+        this.children = [];
+        this.hierarchyNode = null;
     }
 
-    /**
-     * Returns whether the node is the root.
-     */
-    isRoot() {
-        return Object.is(this.parent, null);
-    }
-
-    /**
-     * Adds `newChildren` to `this.children` if the child is not already
-     * present, otherwise merges the feature IDs of `newChild` into the found
-     * child. Returns the newly added child if no match was found, otherwise
-     * returns the match.
-     */
-    addChild(newChild: TaxonomyNode): TaxonomyNode {
-        const matches = this.children.filter((child) => {
-            return child.name == newChild.name;
+    getChild(name: string): TaxonomyNode | null {
+        let matchingChildren = this.children.filter((child) => {
+            return child.name == name;
         });
 
-        if (matches.length == 0) {
-            this.children.push(newChild);
-            return newChild;
-        } else if (matches.length == 1) {
-            const [match] = matches;
-            match.featureIds.push(...newChild.featureIds);
-            return match;
-        } else {
-            throw new Error(`More than one matching child found.`);
+        if (matchingChildren.length > 1) {
+            throw new Error("More than one matching child found.");
         }
-    }
-
-    /**
-     * Returns the node with id `id` if one is present in this node's
-     * descendants, otherwise returns null.
-     */
-    findById(id: string): TaxonomyNode | null {
-        if (this.featureIds.indexOf(id) != -1) {
-            return this;
+        if (matchingChildren.length == 0) {
+            return null;
         }
 
-        for (let child of this.children) {
-            let node = child.findById(id);
-            if (node != null) {
-                return node;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Returns the node(s) with taxon name `name` if any are present in this
-     * node's descendants, otherwise returns null.
-     */
-    findByName(name: string): TaxonomyNode[] | null {
-        let matches = [];
-        if (this.name == name) {
-            matches.push(this);
-        }
-
-        for (let child of this.children) {
-            let nodes = child.findByName(name);
-            if (nodes != null) {
-                matches.push(...nodes);
-            }
-        }
-
-        if (matches.length > 0) {
-            return matches;
-        }
-        return null;
-    }
-
-    /**
-     * Returns the number of ancestors of this node. Can be interpreted as
-     * the level (depth) of this node, with root having level 0.
-     */
-    getNumberAncestors(): number {
-        let ancestorCount = 0;
-        let ancestor = this.parent;
-
-        while (ancestor != null) {
-            ancestor = ancestor.parent;
-            ancestorCount++;
-        }
-
-        return ancestorCount;
-    }
-
-    /**
-     * Returns the number of descendants of this node.
-     */
-    getNumberDescendants(): number {
-        let numberDescendants = 0;
-        for (let child of this.children) {
-            numberDescendants++;
-            numberDescendants += child.getNumberDescendants();
-        }
-
-        return numberDescendants;
+        return matchingChildren[0];
     }
 }
 
 /**
- * Parses a taxonomy.tsv file into a tree of `TaxonomyNode`s.
+ * Parses a taxonomy.tsv file into a d3.hierarchy.
  */
 export async function parseTaxonomy(
     taxonomyFilepath: string,
-): Promise<TaxonomyNode> {
-    const root = new TaxonomyNode("root");
-
+): Promise<d3.HierarchyNode<TaxonomyNode>> {
     const taxonomyRecords = await tsv(taxonomyFilepath);
+
+    const root = new TaxonomyNode("root");
 
     for (let taxonRecord of taxonomyRecords) {
         const levelNames = taxonRecord["Taxon"]
@@ -141,17 +260,31 @@ export async function parseTaxonomy(
 
         let parentNode = root;
         for (let [levelIndex, levelName] of levelNames.entries()) {
-            const currentNode = new TaxonomyNode(levelName, parentNode);
+            const currentNode = new TaxonomyNode(levelName);
 
             // if this is the last level then it has a feature ID
             if (levelIndex == levelNames.length - 1) {
-                currentNode.featureIds = [featureId];
+                currentNode.featureIDs.push(featureId);
             }
 
-            const child = parentNode.addChild(currentNode);
-            parentNode = child;
+            // merge with matching child if one exists
+            const matchingChild = parentNode.getChild(levelName);
+            if (matchingChild) {
+                matchingChild.featureIDs.push(...currentNode.featureIDs);
+                parentNode = matchingChild;
+            } else {
+                parentNode.children.push(currentNode);
+                parentNode = currentNode;
+            }
         }
     }
 
-    return root;
+    const hierarchyRoot = d3.hierarchy(root);
+
+    // create back references from TaxonomyNodes to d3 nodes
+    hierarchyRoot.eachBefore((node) => {
+        node.data.hierarchyNode = node;
+    });
+
+    return hierarchyRoot;
 }
