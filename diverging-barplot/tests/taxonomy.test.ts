9d286aeea29afceba5f760c04825975bf9788249
Colin Wood <woodcolin38@gmail.com>
2025-02-28 15:53:13 -0700

    refactor TaxonomyNode tests

--- diverging-barplot/tests/taxonomy.test.ts
+++ diverging-barplot/tests/taxonomy.test.ts
@@ -1,6 +1,7 @@
 import { vi, describe, test, assert } from "vitest";
 import { readFileSync } from "fs";
 import { TaxonomyNode, parseTaxonomy } from "../src/util/taxonomy";
+import * as d3 from "d3";
 
 // mock d3.tsv
 async function mockedTsv(slice: string): Promise<object[]> {
@@ -27,95 +28,25 @@ vi.mock("d3-fetch", () => {
 });
 
 describe("parseTaxonomy()", async () => {
-    let root: TaxonomyNode;
+    let root: d3.HierarchyNode<TaxonomyNode>;
 
     test("parses successfully", async () => {
         root = await parseTaxonomy("tests/data/taxonomy-fake.tsv");
     });
 
     test("has correct number of nodes", () => {
-        assert.equal(root.getNumberDescendants(), 11);
+        assert.equal(root.descendants().length, 12);
     });
 
     test("root has correct children", () => {
         const exp = ["d1", "d2"];
         assert.deepEqual(
-            root.children.map((c) => c.name),
+            root.children!.map((c) => c.data.name),
             exp,
         );
     });
-});
 
-describe("TaxonomyNode", async () => {
-    const root = await parseTaxonomy("tests/data/taxonomy-fake.tsv");
-
-    test("isRoot", () => {
-        assert.isTrue(root.isRoot());
-
-        const child = root.children[0];
-        assert.isFalse(child.isRoot());
-    });
-
-    test("addChild", () => {
-        assert.equal(root.children.length, 2);
-
-        // new child added, not merged
-        root.addChild(new TaxonomyNode("d3", root, "d3-id1"));
-        assert.equal(root.children.length, 3);
-
-        // existing child merged
-        const child = root.addChild(new TaxonomyNode("d3", root, "d3-id2"));
-        assert.equal(root.children.length, 3);
-        assert.deepEqual(child.featureIds, ["d3-id1", "d3-id2"]);
-    });
-
-    test("findById", () => {
-        let found = root.findById("1");
-        assert.equal(found!.name, "p1");
-
-        found = root.findById("6");
-        assert.equal(found!.name, "p4");
-
-        found = root.findById("ha");
-        assert.isNull(found);
-    });
-
-    test("findByName", () => {
-        let found = root.findByName("root");
-        assert.equal(found!.length, 1);
-        assert.equal(found![0], root);
-
-        found = root.findByName("k1");
-        assert.equal(found!.length, 2);
-
-        found = root.findByName("c1");
-        assert.equal(found!.length, 1);
-        assert.deepEqual(found![0].featureIds, ["7"]);
-
-        found = root.findByName("ha");
-        assert.isNull(found);
-    });
-
-    test("getNumberAncestors", () => {
-        let numAncestors = root.getNumberAncestors();
-        assert.equal(numAncestors, 0);
-
-        const p1 = root.findById("1");
-        numAncestors = p1!.getNumberAncestors();
-        assert.equal(numAncestors, 3);
-
-        const c1 = root.findById("7");
-        numAncestors = c1!.getNumberAncestors();
-        assert.equal(numAncestors, 4);
-    });
-
-    test("getNumberDescendants", () => {
-        const p1 = root.findById("1");
-        let numberDescendants = p1!.getNumberDescendants();
-        assert.equal(numberDescendants, 0);
-
-        const d2 = root.findByName("d2")![0];
-        numberDescendants = d2.getNumberDescendants();
-        assert.equal(numberDescendants, 4);
+    test("back references exist", () => {
+        assert.equal(root.data.hierarchyNode, root);
     });
 });
