7f4a8df9f4f716d1d0083ee9226e5ec0e09b2c85
Colin Wood <woodcolin38@gmail.com>
2025-03-11 14:32:55 -0700

    taxonomy plot layout, selected taxon component

---
+++ diverging-barplot/src/lib/SelectedTaxon.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    const { selectedTaxon } = $props();
+</script>
+
+<div>
+    <p>Name: {selectedTaxon ? selectedTaxon.name : ""}</p>
+    <p>
+        Features classified here:
+        {selectedTaxon ? selectedTaxon.featureIDs.length : ""}
+    </p>
+</div>
+
+<style>
+</style>
--- diverging-barplot/src/lib/Taxonomy.svelte
+++ diverging-barplot/src/lib/Taxonomy.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { TaxonomyPlot, parseTaxonomy } from "../util/taxonomy";
+    import SelectedTaxon from "./SelectedTaxon.svelte";
+    import { TaxonomyPlot, parseTaxonomy } from "../util/taxonomy.svelte";
 
-    let taxonomyPlot: TaxonomyPlot;
+    let taxonomyPlot: TaxonomyPlot | null = null;
     onMount(async () => {
         // parse taxonomy
         const root = await parseTaxonomy("taxonomy.tsv");
@@ -17,13 +18,53 @@
 
 <div class="taxonomy">
     <svg></svg>
-    <div class="selected-taxon"></div>
+    <div class="taxonomy-controls">
+        <div class="selected-taxon">
+            <SelectedTaxon selectedTaxon={taxonomyPlot?.selectedTaxon} />
+        </div>
+        <div class="taxonomy-filter-controls"></div>
+        <div class="taxonomy-filters"></div>
+    </div>
 </div>
 
 <style>
+    .taxonomy {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+
     svg {
+        height: 70vh;
+        width: 80vw;
         border: 2px solid lightgray;
         border-radius: 5px;
-        min-height: 750px;
+    }
+
+    .taxonomy-controls {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        width: 18vw;
+    }
+
+    .selected-taxon {
+        width: 100%;
+        height: 40%;
+        border: 2px solid lightgray;
+        border-radius: 5px;
+    }
+    .taxonomy-filter-controls {
+        width: 100%;
+        height: 28%;
+        border: 2px solid lightgray;
+        border-radius: 5px;
+    }
+    .taxonomy-filters {
+        width: 100%;
+        height: 28%;
+        border: 2px solid lightgray;
+        border-radius: 5px;
     }
 </style>
